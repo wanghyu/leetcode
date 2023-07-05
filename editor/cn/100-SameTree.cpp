bc81d30162248bfa5590ef7d23f8eb0a21e083bd
wanghyu <wanghyu@163.com>
2023-07-05 23:27:16 +0800

    2023-07-05

---
+++ editor/cn/100-SameTree.cpp
@@ -0,0 +1,67 @@
+//Given the roots of two binary trees p and q, write a function to check if 
+//they are the same or not. 
+//
+// Two binary trees are considered the same if they are structurally identical, 
+//and the nodes have the same value. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: p = [1,2,3], q = [1,2,3]
+//Output: true
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: p = [1,2], q = [1,null,2]
+//Output: false
+// 
+//
+// Example 3: 
+// 
+// 
+//Input: p = [1,2,1], q = [1,1,2]
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in both trees is in the range [0, 100]. 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1035 👎 0
+
+
+#include "leetcode-base.h"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main(){
+    [[maybe_unused]] Solution s;
+    
+    return 0;
+}
